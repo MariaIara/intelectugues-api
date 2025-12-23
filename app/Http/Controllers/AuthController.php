29f23c7a12d1740a5f007c18c480aa9af7b58372
Iara Braga <iara@incibrasil.com.br>
2025-12-22 21:16:42 -0300

    feat: login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -16,6 +18,24 @@ class AuthController extends Controller
             ...$validated,
             'password' => Hash::make($validated['password'])
         ]);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'user' => 'Invalid credentials.'
+            ]);
+        }
 
         return response()->json([
             'user' => $user,
